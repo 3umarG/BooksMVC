fa571267ad8a56f1184b932b2fa51b1b9b18e5da
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-06-26 03:35:37 +0200

    Added Initial Migration

---
+++ BooksWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BooksWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ BooksWebApp/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksWebApp.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
