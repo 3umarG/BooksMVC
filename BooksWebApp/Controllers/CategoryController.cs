a602277048c3825a8eab9f969c91e30afc6131e7
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-06-26 06:21:14 +0200

    Added Create Action Functionality with Server Side Validation

--- BooksWebApp/Controllers/CategoryController.cs
+++ BooksWebApp/Controllers/CategoryController.cs
@@ -18,9 +18,30 @@ namespace BooksWebApp.Controllers
             return View(categories);
         }
 
+        /// GET View of the Form
         public IActionResult Create()
         {
             return View();
         }
-    }
+
+
+        /// POST the result of the Form <summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public IActionResult Create(Category category)
+		{
+            // Server Side Validation 
+            if (category.DisplayOrder.ToString().Equals(category.Name))
+            {
+                ModelState.AddModelError("name", "The Category Name cannot exactly the same as DisplayOrder !!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+			return View(category);
+		}
+	}
 }
--- BooksWebApp/Models/Category.cs
+++ BooksWebApp/Models/Category.cs
@@ -9,7 +9,7 @@ namespace BooksWebApp.Models
         [Required]
         public string Name { get; set; }
 
-
+        [Range(1 , 100)]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
